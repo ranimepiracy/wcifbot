e7c039aa129c2ebbe581dd88a8b38a4ee96a56f6
Anonymous <anon@ymo.us>
2020-10-21 15:24:09 -0400

    Implemented WIP stream API lookup and comment responding.

---
+++ config.example.py
@@ -0,0 +1,13 @@
+# Reddit / PRAW
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+USER_AGENT = ""
+USERNAME = ""
+PASSWORD = ""
+
+SUBREDDIT = ""
+THREAD_ID = ""
+CRON_INTERVAL = 10
+
+# MALSync Streams API
+API = "https://api.malsync.moe/mal/{type}/{id}"
